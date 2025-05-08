7b1f248bcc06b17db6486848575a4415ce670dc1
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-08 10:23:11 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import {useLocation} from "react-router-dom"
-const App = () => {
+import React from "react";
+import Navbar from "./Components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
 
-    const isownerpath=useLocation().pathname.includes("owner")
+const App = () => {
+  const isownerpath = useLocation().pathname.includes("owner");
+
   return (
     <div>
-      {!isownerpath && <Navbar/>}
+      {!isownerpath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
