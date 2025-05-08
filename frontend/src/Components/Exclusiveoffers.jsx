50058cabf87b41e4e77d120b67e9ff17b701c6e0
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-08 17:29:53 +0530

    done

--- frontend/src/Components/Exclusiveoffers.jsx
+++ frontend/src/Components/Exclusiveoffers.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets, exclusiveOffers } from '../assets/assets'
 import Title from './Title'
+
 const Exclusiveoffers = () => {
   return (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 pt-20 pb-30'>
       <div className='flex flex-col md:flex-row items-center justify-between w-full'>
-        <Title align='left' title='Exclusive Offers' subtitle='Take advantage of our limited-offers and special packages to enhance your stay and create unforgettable memories'/>
-        <button>View All Offers
-            <img src={assets.arrowIcon} alt="" className='group-hover:translate-x-1 transition-all'/>
+        <Title
+          align='left'
+          title='Exclusive Offers'
+          subtitle='Take advantage of our limited-offers and special packages to enhance your stay and create unforgettable memories'
+        />
+        <button className='group flex items-center gap-2 font-medium cursor-pointer max-md:mt-12'>
+          View All Offers
+          <img src={assets.arrowIcon} alt="" className='group-hover:translate-x-1 transition-all'/>
         </button>
+      </div>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full mt-12'>
+        {exclusiveOffers.map((item) => (
+          <div
+            key={item._id}
+            className='group relative flex flex-col items-start justify-between gap-1 h-60 px-4 py-6 rounded-xl text-white bg-no-repeat bg-cover bg-center'
+            style={{ backgroundImage: `url(${item.image})` }}
+          >
+            <p className='px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full'>
+              {item.priceOff}% OFF
+            </p>
+            </div>
+            ))}
+            <div className='mt-auto'>
+              <h3 className='text-lg font-semibold'>{item.title}</h3>
+              <p className='text-sm text-white/90'>{item.description}</p>
+          </div>
       </div>
     </div>
   )
