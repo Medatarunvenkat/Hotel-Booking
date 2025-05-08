b4951a74e8829f35a035835dea1089640cd9393d
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-08 16:30:06 +0530

    now

--- frontend/src/Components/HotelCard.jsx
+++ frontend/src/Components/HotelCard.jsx
@@ -14,12 +14,12 @@ const HotelCard = ({room,index}) => {
                 <img src={assets.starIconFilled} alt="" />
             </div>
         </div>
-        <div>
+        <div className='flex items-center gap-1 text-sm'>
         <img src={assets.locationIcon} alt="" />
         <span>{room.hotel.address}</span>
         </div>
-        <div>
-            <p><span>${room.pricepernight}</span>/night</p>
+        <div className='flex items-center justify-between mt-4'>
+            <p><span className='text-xl text-gray-800'>${room.pricepernight}</span>/night</p>
             <button className='px-4 py-2 text-sm font-medium border border-gray-300 rounded hover:bg-gray-50 transition-all cursor-pointer'>Book Now</button>
         </div>
       </div>
