596dad5619198aafed802361cc87f30be82d0e5b
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-09 08:54:17 +0530

    done

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../Components/Hero"; // correct Hero path
 import FeaturedDestination from "../Components/FeaturedDestination";
 import Exclusiveoffers from "../Components/Exclusiveoffers";
+import Testimonial from "../Components/Testimonial";
 const Home = () => {
   return (
     <div>
       <Hero />
       <FeaturedDestination/>
       <Exclusiveoffers/>
+      <Testimonial/>
     </div>
   );
 };
