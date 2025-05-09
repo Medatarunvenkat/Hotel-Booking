b91681e1240f492ce0416f0a8d05d59553dab381
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-09 22:41:40 +0530

    address added

--- frontend/src/Pages/RoomDetails.jsx
+++ frontend/src/Pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { roomsDummyData } from '../assets/assets'
+import { assets, roomsDummyData } from '../assets/assets'
 const RoomDetails = () => {
     const {id}=useParams()
     const[room,setroom]=useState(null)
@@ -16,9 +16,18 @@ const RoomDetails = () => {
             <h1 className='text-3xl md:text-4xl'>{room.hotel.name} <span className='font-inter text-sm'>({room.roomType})</span></h1>
             <p className='text-xs font-inter py-1.5 px-3 text-white bg-orange-500 rounded-full '>20% OFF</p>
         </div>
-        <div>
-            
+
+
+        <div className='flex items-center gap-1 mt-2'>
+             <p className='text-yellow-500'>★★★★★</p>
+             <p className='ml-2'>200+ Reviews</p>
         </div>
+
+        <div className='flex items-center gap-1 text-gray-500 mt-2'>
+            <img src={assets.locationIcon} alt="" />
+            <span>{room.hotel.address}</span>
+        </div>
+
     </div>
   )
 }
