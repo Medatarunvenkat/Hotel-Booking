a860e52ff866cdadb6b2625871754f19b4531ac6
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-09 08:43:54 +0530

    done

---
+++ frontend/src/Components/Testimonial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Title from "./Title"
+const Testimonial = () => {
+  return (
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 pt-20 pb-30'>
+      <Title title='What our Guests say' subtitle='Discover why discerning travelers consistently choose Tarun vitara Grand for their exclusive and luxurious accomodations around the world'/>
+    </div>
+  )
+}
+
+export default Testimonial
