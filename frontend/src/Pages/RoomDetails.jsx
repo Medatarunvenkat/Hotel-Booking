f59c7174f1d91723a06847fb4ac3c3c20dc87171
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-10 08:32:45 +0530

    button created

--- frontend/src/Pages/RoomDetails.jsx
+++ frontend/src/Pages/RoomDetails.jsx
@@ -55,6 +55,12 @@ const RoomDetails = () => {
 
         <form className='flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0,15) p-6 rounded-xl mx-auto mt-16 max-w-6xl]'>
 
+
+
+        <button className='bg-primary hover:bg-primary-dull active:scale-95 transition-all text-white rounded-md max-md:w-full max-md:mt-6 md:px-25 py-3 md:py-4 text-base cursor-pointer'>
+            Book Now
+        </button>
+
         </form>
 
 
