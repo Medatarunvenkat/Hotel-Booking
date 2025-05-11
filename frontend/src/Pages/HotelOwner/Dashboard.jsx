73e8ae76e2e9894feeb57f8c000da6d4c3d8dc5d
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-11 16:38:08 +0530

    owner dashboard started

--- frontend/src/Pages/HotelOwner/Dashboard.jsx
+++ frontend/src/Pages/HotelOwner/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Title from "../../Components/Title"
 const Dashboard = () => {
   return (
     <div>
-      
+      <Title align='left' font='outfit' title='DashBoard' subtitle='Monitor your '/>
     </div>
   )
 }
