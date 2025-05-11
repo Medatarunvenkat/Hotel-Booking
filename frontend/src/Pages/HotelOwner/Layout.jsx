792b62a698bb43787185c4ea0104ae925191e57c
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-11 16:30:53 +0530

    sidebar gngon

--- frontend/src/Pages/HotelOwner/Layout.jsx
+++ frontend/src/Pages/HotelOwner/Layout.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from '../../Components/HotelOwner/Navbar'
+import Sidebar from '../../Components/HotelOwner/Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <div className='flex flex-col h-screen'>
         <Navbar/>
+        <div className='flex h-full'>
+          <Sidebar/>
+          <div className='flex-1 p-4'>
+            <Outlet/>
+          </div>
+        </div>
     </div>
   )
 }
